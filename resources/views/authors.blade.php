c9958ca8bb6e6a09bc64796ddda784eb0a18ff81
Elnath <farrosrifantiarno32@gmail.com>
2025-08-04 22:58:45 +0700

    create authors page

---
+++ resources/views/authors.blade.php
@@ -0,0 +1,17 @@
+<x-layout :title="$title">
+    <a href="/posts" class="text-blue-400 hover:text-blue-600 hover:underline">&laquo; Kembali</a>
+    @foreach ($posts as $post)
+        <article class="py-8 max-w-screen-md border-b border-gray-300"> 
+            <a href="/posts/{{ $post['slug'] }}" class="font-bold text-gray-900 hover:underline decoration-2">
+                <h2 class="mb-1 text-3xl tracking-tight">{{ $post['title'] }}</h2>
+            </a>
+            <div class="text-base text-gray-500">
+                <a href="/authors/{{ $post->author->id }}" class="hover:underline">{{ $post->author->name }}</a> | {{ $post['created_at'] }}
+            </div>
+            <p class="text-justify my-3 font-light">
+                {{ Str::limit($post['content'], 100) }}
+            </p>
+            <a href="/posts/{{ $post['slug'] }}" class="font-medium text-blue-400 hover:text-blue-700 hover:underline">Read More &raquo;</a>
+        </article>
+    @endforeach
+</x-layout>
